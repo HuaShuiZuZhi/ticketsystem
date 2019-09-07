05505665f82996974f54e452cc1f4e92fbdf8a2e
xhui <xiehui88i888@gmail.com>
2019-09-08 00:34:19 +0800

    不完善的页面结构

--- src/main.js
+++ src/main.js
@@ -2,7 +2,38 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import {Button} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Container,
+  Aside,
+  Menu,
+  MenuItem,
+  MenuItemGroup,
+  Submenu,
+  Header,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Main,
+  Table,
+  TableColumn,
+  Button} from 'element-ui';
+
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
+Vue.use(Header)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Main)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Button)
+
 
 Vue.config.productionTip = false
 
